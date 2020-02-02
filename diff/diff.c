7e9bbcb1ef2e03f2a705df061c393967c13c756a
devvrr <60576481+devvrr@users.noreply.github.com>
2020-02-02 21:16:57 +0530

    Create diff.c

---
+++ diff/diff.c
@@ -0,0 +1,115 @@
+/*Implements the Rubik cube configuration difference function*/
+#include "../rbdefs.h"
+
+/*difference of corner cubelet direction values
+ * diff_dir = dirMap[c2_dir][c1_dir]*/
+int dirMap[3][3] = {{0,1,2},{2,0,1},{1,2,0}};
+
+/*The Rubik Cube configuration difference function:
+ * The difference of two configs is such that
+ * relative placement of cubelets in "diff" relative to "Goal" is same as the relative
+ * placement of cubelets in "c2" compared to "c1"*/
+void RbCubeConfigDiff( rbCubeState* config1, rbCubeState* config2, rbCubeState* diff)
+{
+   int i,j;
+   for( i=0; i<12; i++)
+   {
+      for(j=0;j<12;j++)
+      {
+	 if( config1->edgePos[i] == config2->edgePos[j] )
+	 {
+	    diff->edgePos[j] = i;
+	    if( config1->edgeDir[i] == config2->edgeDir[j] )
+	       diff->edgeDir[j] = 0;
+	    else
+	       diff->edgeDir[j] = 1;
+	    break;
+	 }
+      }
+   }
+
+   for( i=0; i<8; i++ )
+   {
+      for( j=0; j<8; j++)
+      {
+	 if( config1->cornerPos[i] == config2->cornerPos[j] )
+	 {
+	    diff->cornerPos[j] = i;
+	    diff->cornerDir[j] = dirMap[config1->cornerDir[i]][config2->cornerDir[j]];
+	    break;
+	 }
+      }
+   }
+}
+void PrintEdgeColors( void )
+{
+   printf("\n              rg  rb  gp  pb  yb  gy  gw  wb  py  pw wr  ry");
+}
+void PrintCornerColors( void )
+{
+   printf("\n\n             gpy rgw bpw ryb rwb rgy wgp ybp");
+}
+static void PrintRbCubeConfig( rbCubeState * s )
+{
+   UINT8 i;
+   printf("the config is\n");
+   PrintEdgeColors();
+   printf("\nedge pos:  ");
+   for(i=0;i<12;i++)
+      printf("%4d",(s->edgePos[i]));
+   printf("\nedge dir:  ");
+   for(i=0;i<12;i++)
+      printf("%4d",(s->edgeDir[i]));
+
+   PrintCornerColors();
+   printf("\ncorner pos:");
+   for(i=0;i<8;i++)
+      printf("%4d",(s->cornerPos[i]));
+   
+   printf("\ncorner dir:");
+   for(i=0;i<8;i++)
+      printf("%4d",(s->cornerDir[i]));
+}
+static void ReadRbCubeConfig( rbCubeState * c )
+{
+   UINT8 i;
+   int edgePos,edgeDir,cornerPos,cornerDir;
+   printf("enter edge pos values\n");
+   for(i=0;i<12;i++)
+   {
+	   scanf("%d",&edgePos);
+	   c->edgePos[i] = edgePos;
+   }
+   printf("enter edge dir values\n");
+   for(i=0;i<12;i++)
+   {
+	   scanf("%d",&edgeDir);
+	   c->edgeDir[i]=edgeDir;
+   }
+   printf("enter corner pos values\n");
+   for(i=0;i<8;i++)
+   {
+	   scanf("%d",&cornerPos);
+	   c->cornerPos[i]=cornerPos;
+   }
+   printf("enter corner dir values\n");
+   for(i=0;i<8;i++)
+   {
+	   scanf("%d",&cornerDir);
+	   c->cornerDir[i]=cornerDir;
+   }
+}
+#if 0
+void main( void )
+{
+   rbCubeState c1, c2, diff;
+   printf("config 1\n");
+   ReadRbCubeConfig(&c1);
+   printf("config 2\n");
+   ReadRbCubeConfig(&c2);
+   RbCubeConfigDiff(&c1, &c2, &diff);
+
+   printf("\n\nthe difference config is:\n");
+   PrintRbCubeConfig( &diff );
+}
+#endif
