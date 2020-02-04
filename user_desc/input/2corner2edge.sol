c7c50e78e0860dcbe24593a7e48a01a5d5dc78fa
devvrr <60576481+devvrr@users.noreply.github.com>
2020-02-04 22:14:14 +0530

    Create 2corner2edge.sol

---
+++ user_desc/input/2corner2edge.sol
@@ -0,0 +1,8 @@
+  edge pos:  0  1  2  9  4  5  6  7  8  3 10 11
+  edge dir:  0  0  0  1  0  0  0  0  0  1  0  0
+corner pos:  0  1  7  3  4  5  6  2
+corner dir:  0  0  0  0  0  0  0  0
+
+*****full path
+pink-in -blue-in -pink-in -pink-in -white-in -pink-out - ---  --- blue-out -pink-in -blue-in -pink-in -white-out -pink-in -blue-out -
+Path Length:13
