9fe81319a98f393185164fec20f1f7f23b3f0e90
devvrr <60576481+devvrr@users.noreply.github.com>
2020-08-22 21:09:26 +0530

    Update edge_pos_dir.c

--- child_gen/edge_pos_dir.c
+++ child_gen/edge_pos_dir.c
@@ -28,130 +28,86 @@ void Find_child_edge_pos( rbCubeState * parent, rbCubeState * child, UINT8 move 
 
 	for( i=0;i<NEDGECUBELETS;i++){
 		child->edgePos[i] = parent->edgePos[i];
-		child->edgeDir[i] = parent->edgeDir[i];
 	}
-
+#define EDIR 0x10
+            /*5th bit is the direction value. so flip it to change it*/
 	switch( move ){
 		case GREEN_IN: /*green in*/
-			child->edgePos[green_yellow] =  parent->edgePos[green_red];
-			child->edgeDir[green_yellow] = !parent->edgeDir[green_red];
-			child->edgePos[green_red] =     parent->edgePos[green_white];
-			child->edgeDir[green_red] =    !parent->edgeDir[green_white];
-			child->edgePos[green_white] =   parent->edgePos[green_pink];
-			child->edgeDir[green_white] =  !parent->edgeDir[green_pink];
-			child->edgePos[green_pink] =    parent->edgePos[green_yellow];
-			child->edgeDir[green_pink] =   !parent->edgeDir[green_yellow];
+            //printf("green in\n");
+            //printf("parent->edgePos[green_red]=%d\n",parent->edgePos[green_red]);
+			child->edgePos[green_yellow] =  parent->edgePos[green_red] ^ EDIR;
+			child->edgePos[green_red] =     parent->edgePos[green_white] ^ EDIR;
+			child->edgePos[green_white] =   parent->edgePos[green_pink] ^ EDIR;
+			child->edgePos[green_pink] =    parent->edgePos[green_yellow] ^ EDIR;
 			break;
 		case GREEN_OUT: /*green out*/
-			child->edgePos[green_yellow] =  parent->edgePos[green_pink];
-			child->edgeDir[green_yellow] = !parent->edgeDir[green_pink];
-			child->edgePos[green_pink] =    parent->edgePos[green_white];
-			child->edgeDir[green_pink] =   !parent->edgeDir[green_white];
-			child->edgePos[green_white] =   parent->edgePos[green_red];
-			child->edgeDir[green_white] =  !parent->edgeDir[green_red];
-			child->edgePos[green_red] =     parent->edgePos[green_yellow];
-			child->edgeDir[green_red] =    !parent->edgeDir[green_yellow];
+            //printf("green out\n");
+            //printf("parent->edgePos[green_pink]=%d\n",parent->edgePos[green_pink]);
+			child->edgePos[green_yellow] =  parent->edgePos[green_pink] ^ EDIR;
+			child->edgePos[green_pink] =    parent->edgePos[green_white] ^ EDIR;
+			child->edgePos[green_white] =   parent->edgePos[green_red] ^ EDIR;
+			child->edgePos[green_red] =     parent->edgePos[green_yellow] ^ EDIR;
 
 			break;
 		case BLUE_IN: /*blue in*/
-			child->edgePos[blue_yellow] =  parent->edgePos[blue_pink];
-			child->edgeDir[blue_yellow] = !parent->edgeDir[blue_pink];
-			child->edgePos[blue_pink] =    parent->edgePos[blue_white];
-			child->edgeDir[blue_pink] =   !parent->edgeDir[blue_white];
-			child->edgePos[blue_white] =   parent->edgePos[blue_red];
-			child->edgeDir[blue_white] =  !parent->edgeDir[blue_red];
-			child->edgePos[blue_red] =     parent->edgePos[blue_yellow];
-			child->edgeDir[blue_red] =    !parent->edgeDir[blue_yellow];
+			child->edgePos[blue_yellow] =  parent->edgePos[blue_pink] ^ EDIR;
+			child->edgePos[blue_pink] =    parent->edgePos[blue_white] ^ EDIR;
+			child->edgePos[blue_white] =   parent->edgePos[blue_red] ^ EDIR;
+			child->edgePos[blue_red] =     parent->edgePos[blue_yellow] ^ EDIR;
 			break;
 		case BLUE_OUT: /*blue out*/
-			child->edgePos[blue_yellow] =  parent->edgePos[blue_red];
-			child->edgeDir[blue_yellow] = !parent->edgeDir[blue_red];
-			child->edgePos[blue_red] =     parent->edgePos[blue_white];
-			child->edgeDir[blue_red] =    !parent->edgeDir[blue_white];
-			child->edgePos[blue_white] =   parent->edgePos[blue_pink];
-			child->edgeDir[blue_white] =  !parent->edgeDir[blue_pink];
-			child->edgePos[blue_pink] =    parent->edgePos[blue_yellow];
-			child->edgeDir[blue_pink] =   !parent->edgeDir[blue_yellow];
+			child->edgePos[blue_yellow] =  parent->edgePos[blue_red] ^ EDIR;
+			child->edgePos[blue_red] =     parent->edgePos[blue_white] ^ EDIR;
+			child->edgePos[blue_white] =   parent->edgePos[blue_pink] ^ EDIR;
+			child->edgePos[blue_pink] =    parent->edgePos[blue_yellow] ^ EDIR;
 			break;
 		case RED_IN: /*red in*/
-			child->edgePos[red_white] =   parent->edgePos[red_green];
-			child->edgeDir[red_white] =  !parent->edgeDir[red_green];
-			child->edgePos[red_green] =   parent->edgePos[red_yellow];
-			child->edgeDir[red_green] =  !parent->edgeDir[red_yellow];
-			child->edgePos[red_yellow] =  parent->edgePos[red_blue];
-			child->edgeDir[red_yellow] = !parent->edgeDir[red_blue];
-			child->edgePos[red_blue] =    parent->edgePos[red_white];
-			child->edgeDir[red_blue] =   !parent->edgeDir[red_white];
+			child->edgePos[red_white] =   parent->edgePos[red_green] ^ EDIR;
+			child->edgePos[red_green] =   parent->edgePos[red_yellow] ^ EDIR;
+			child->edgePos[red_yellow] =  parent->edgePos[red_blue] ^ EDIR;
+			child->edgePos[red_blue] =    parent->edgePos[red_white] ^ EDIR;
 			break;
 		case RED_OUT: /*red out*/
-			child->edgePos[red_white] =   parent->edgePos[red_blue];
-			child->edgeDir[red_white] =  !parent->edgeDir[red_blue];
-			child->edgePos[red_blue] =    parent->edgePos[red_yellow];
-			child->edgeDir[red_blue] =   !parent->edgeDir[red_yellow];
-			child->edgePos[red_yellow] =  parent->edgePos[red_green];
-			child->edgeDir[red_yellow] = !parent->edgeDir[red_green];
-			child->edgePos[red_green] =   parent->edgePos[red_white];
-			child->edgeDir[red_green] =  !parent->edgeDir[red_white];
+			child->edgePos[red_white] =   parent->edgePos[red_blue] ^ EDIR;
+			child->edgePos[red_blue] =    parent->edgePos[red_yellow] ^ EDIR;
+			child->edgePos[red_yellow] =  parent->edgePos[red_green] ^ EDIR;
+			child->edgePos[red_green] =   parent->edgePos[red_white] ^ EDIR;
 			break;
 		case PINK_IN: /*pink in*/
-			child->edgePos[pink_white] =   parent->edgePos[pink_blue];
-			child->edgeDir[pink_white] =  !parent->edgeDir[pink_blue];
-			child->edgePos[pink_blue] =    parent->edgePos[pink_yellow];
-			child->edgeDir[pink_blue] =   !parent->edgeDir[pink_yellow];
-			child->edgePos[pink_yellow] =  parent->edgePos[pink_green];
-			child->edgeDir[pink_yellow] = !parent->edgeDir[pink_green];
-			child->edgePos[pink_green] =   parent->edgePos[pink_white];
-			child->edgeDir[pink_green] =  !parent->edgeDir[pink_white];
+			child->edgePos[pink_white] =   parent->edgePos[pink_blue] ^ EDIR;
+			child->edgePos[pink_blue] =    parent->edgePos[pink_yellow] ^ EDIR;
+			child->edgePos[pink_yellow] =  parent->edgePos[pink_green] ^ EDIR;
+			child->edgePos[pink_green] =   parent->edgePos[pink_white] ^ EDIR;
 			break;
 		case PINK_OUT: /*pink out*/
-			child->edgePos[pink_white] =   parent->edgePos[pink_green];
-			child->edgeDir[pink_white] =  !parent->edgeDir[pink_green];
-			child->edgePos[pink_green] =   parent->edgePos[pink_yellow];
-			child->edgeDir[pink_green] =  !parent->edgeDir[pink_yellow];
-			child->edgePos[pink_yellow] =  parent->edgePos[pink_blue];
-			child->edgeDir[pink_yellow] = !parent->edgeDir[pink_blue];
-			child->edgePos[pink_blue] =    parent->edgePos[pink_white];
-			child->edgeDir[pink_blue] =   !parent->edgeDir[pink_white];
+			child->edgePos[pink_white] =   parent->edgePos[pink_green] ^ EDIR;
+			child->edgePos[pink_green] =   parent->edgePos[pink_yellow] ^ EDIR;
+			child->edgePos[pink_yellow] =  parent->edgePos[pink_blue] ^ EDIR;
+			child->edgePos[pink_blue] =    parent->edgePos[pink_white] ^ EDIR;
 			break;
 		case WHITE_IN: /*white in*/
-			child->edgePos[white_blue] =   parent->edgePos[white_pink];
-			child->edgeDir[white_blue] =  !parent->edgeDir[white_pink];
-			child->edgePos[white_pink] =   parent->edgePos[white_green];
-			child->edgeDir[white_pink] =  !parent->edgeDir[white_green];
-			child->edgePos[white_green] =  parent->edgePos[white_red];
-			child->edgeDir[white_green] = !parent->edgeDir[white_red];
-			child->edgePos[white_red] =    parent->edgePos[white_blue];
-			child->edgeDir[white_red] =   !parent->edgeDir[white_blue];
+			child->edgePos[white_blue] =   parent->edgePos[white_pink] ^ EDIR;
+			child->edgePos[white_pink] =   parent->edgePos[white_green] ^ EDIR;
+			child->edgePos[white_green] =  parent->edgePos[white_red] ^ EDIR;
+			child->edgePos[white_red] =    parent->edgePos[white_blue] ^ EDIR;
 			break;
 		case WHITE_OUT: /*white out*/
-			child->edgePos[white_blue] =   parent->edgePos[white_red];
-			child->edgeDir[white_blue] =  !parent->edgeDir[white_red];
-			child->edgePos[white_red] =    parent->edgePos[white_green];
-			child->edgeDir[white_red] =   !parent->edgeDir[white_green];
-			child->edgePos[white_green] =  parent->edgePos[white_pink];
-			child->edgeDir[white_green] = !parent->edgeDir[white_pink];
-			child->edgePos[white_pink] =   parent->edgePos[white_blue];
-			child->edgeDir[white_pink] =  !parent->edgeDir[white_blue];
+			child->edgePos[white_blue] =   parent->edgePos[white_red] ^ EDIR;
+			child->edgePos[white_red] =    parent->edgePos[white_green] ^ EDIR;
+			child->edgePos[white_green] =  parent->edgePos[white_pink] ^ EDIR;
+			child->edgePos[white_pink] =   parent->edgePos[white_blue] ^ EDIR;
 			break;
 		case YELLOW_IN: /*yellow in*/
-			child->edgePos[yellow_green] =  parent->edgePos[yellow_pink];
-			child->edgeDir[yellow_green] = !parent->edgeDir[yellow_pink];
-			child->edgePos[yellow_pink] =   parent->edgePos[yellow_blue];
-			child->edgeDir[yellow_pink] =  !parent->edgeDir[yellow_blue];
-			child->edgePos[yellow_blue] =   parent->edgePos[yellow_red];
-			child->edgeDir[yellow_blue] =  !parent->edgeDir[yellow_red];
-			child->edgePos[yellow_red] =    parent->edgePos[yellow_green];
-			child->edgeDir[yellow_red] =   !parent->edgeDir[yellow_green];
+			child->edgePos[yellow_green] =  parent->edgePos[yellow_pink] ^ EDIR;
+			child->edgePos[yellow_pink] =   parent->edgePos[yellow_blue] ^ EDIR;
+			child->edgePos[yellow_blue] =   parent->edgePos[yellow_red] ^ EDIR;
+			child->edgePos[yellow_red] =    parent->edgePos[yellow_green] ^ EDIR;
 			break;
 		case YELLOW_OUT: /*yellow out*/
-			child->edgePos[yellow_green] =  parent->edgePos[yellow_red];
-			child->edgeDir[yellow_green] = !parent->edgeDir[yellow_red];
-			child->edgePos[yellow_red] =    parent->edgePos[yellow_blue];
-			child->edgeDir[yellow_red] =   !parent->edgeDir[yellow_blue];
-			child->edgePos[yellow_blue] =   parent->edgePos[yellow_pink];
-			child->edgeDir[yellow_blue] =  !parent->edgeDir[yellow_pink];
-			child->edgePos[yellow_pink] =   parent->edgePos[yellow_green];
-			child->edgeDir[yellow_pink] =  !parent->edgeDir[yellow_green];
+			child->edgePos[yellow_green] =  parent->edgePos[yellow_red] ^ EDIR;
+			child->edgePos[yellow_red] =    parent->edgePos[yellow_blue] ^ EDIR;
+			child->edgePos[yellow_blue] =   parent->edgePos[yellow_pink] ^ EDIR;
+			child->edgePos[yellow_pink] =   parent->edgePos[yellow_green] ^ EDIR;
 			break;
 		default:
 			printf("error\n");
